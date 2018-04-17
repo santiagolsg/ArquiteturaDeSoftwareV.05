e8d58fed6f1d760bb22a7dc29def90ec503f54ae
santiagolsg <36718402+santiagolsg@users.noreply.github.com>
2018-04-17 14:54:27 -0300

    AULA 05 ServiceDesk - Json e fechar chamado(TENTATIVA)

---
+++ src/src/br/usjt/arqsw/service/ChamadoService.java
@@ -0,0 +1,47 @@
+package br.usjt.arqsw.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.usjt.arqsw.dao.ChamadoDAO;
+import br.usjt.arqsw.entity.Chamado;
+import br.usjt.arqsw.entity.Fila;
+
+/**
+ * 
+ *  @author LEONARDO SANTIAGO GONÇALVES 81612334 SIN3AM-MCA
+ */
+
+@Service
+public class ChamadoService {
+	ChamadoDAO dao;
+	
+	@Autowired
+	public ChamadoService(ChamadoDAO dao){
+		this.dao = dao;
+	}
+	
+	public int novoChamado(Chamado chamado) throws IOException{
+		chamado.setDt_abertura(new Date());
+		chamado.setDt_fechamento(null);
+		chamado.setStatus(Chamado.ABERTO);
+		return -1;
+		//return dao.inserirChamado(chamado);
+	}
+
+	public List<Chamado> listarChamados(Fila fila) throws IOException{
+		return dao.listarChamados(fila);
+	}
+
+	public List <Chamado> listarChamados()throws IOException{
+		return dao.listarChamados();
+	}
+	public void fecharChamados(ArrayList<Integer> lista) throws IOException{
+		dao.fecharChamados(lista);
+	}
+}
